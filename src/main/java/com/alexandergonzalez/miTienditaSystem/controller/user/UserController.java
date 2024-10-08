6c72507911271cc33bc024de9d12306fec58524b
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-07 22:41:02 -0600

    Cambios en los endpoints y su lógica

--- src/main/java/com/alexandergonzalez/miTienditaSystem/controller/user/UserController.java
+++ src/main/java/com/alexandergonzalez/miTienditaSystem/controller/user/UserController.java
@@ -1,8 +1,8 @@
 package com.alexandergonzalez.miTienditaSystem.controller.user;
 
-import com.alexandergonzalez.miTienditaSystem.models.user.User;
-import com.alexandergonzalez.miTienditaSystem.models.user.UserDto;
-import com.alexandergonzalez.miTienditaSystem.service.user.UserServiceMap;
+import com.alexandergonzalez.miTienditaSystem.entity.User;
+import com.alexandergonzalez.miTienditaSystem.dto.UserDto;
+import com.alexandergonzalez.miTienditaSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,52 +13,48 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/users/")
+@RequestMapping("/v1/user/")
 
 public class UserController {
 
-    private final UserServiceMap userServiceMap;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserServiceMap userServiceMap) {
-        this.userServiceMap = userServiceMap;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     // Endpoint para poder crear un nuevo usuario
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
-        User user = new User(userDto);
-        User userSaved = userServiceMap.saveUser(user);
-        URI createdUserURI = URI.create("/v1/user/" + userSaved.getId());
-        return ResponseEntity.created(createdUserURI).build();
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto user = userService.saveUser(userDto);
+        return ResponseEntity.ok(user);
     }
 
     // Endpoint que devolverá todos los usuarios exisntentes en la base de datos
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userServiceMap.getALlUsers();
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = userService.getALlUsers();
         return ResponseEntity.ok(users);
     }
 
     // Endpoint que devuelve un usuario por su id
     @GetMapping("{id}")
-    public ResponseEntity<User> findById(@PathVariable String id) {
-        Optional<User> userFound = userServiceMap.findUserById(id);
-        if(userFound.isPresent()){
-            return ResponseEntity.ok(userFound.get());
+    public ResponseEntity<UserDto> findById(@PathVariable String id) {
+        UserDto userFound = userService.findUserById(id);
+        if(userFound != null){
+            return ResponseEntity.ok(userFound);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     // Endpoint que actualiza un usuario por su id
     @PutMapping("{id}")
-    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody UserDto userDto) {
-        Optional<User> userFound = userServiceMap.findUserById(id);
-        if(userFound.isPresent()){
-            User userToUpdate = userFound.get();
-            userToUpdate.update(userDto);
-            userServiceMap.saveUser(userToUpdate);
-            return ResponseEntity.ok(userToUpdate);
+    public ResponseEntity<UserDto> updateUser(@PathVariable String id, @RequestBody UserDto userDto) {
+        UserDto userFound = userService.findUserById(id);
+        if(userFound != null){
+            UserDto updatedUser = userService.updateUser(id, userDto);
+            return ResponseEntity.ok(updatedUser);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
@@ -66,9 +62,9 @@ public class UserController {
     // Endpoint que elimina un usuario existente por su id
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable String id) {
-        Optional<User> userFound = userServiceMap.findUserById(id);
-        if(userFound.isPresent()){
-            userServiceMap.deleteUser(id);
+      UserDto userFound = userService.findUserById(id);
+        if(userFound != null){
+            userService.deleteUser(id);
             return ResponseEntity.ok().build();
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
