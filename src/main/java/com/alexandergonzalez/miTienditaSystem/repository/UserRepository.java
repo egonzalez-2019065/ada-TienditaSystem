6e6a41f393e0e6fca87200802b00f1b797c0168b
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-11 21:19:28 -0600

    implementación: usuario por nombre de usuario

--- src/main/java/com/alexandergonzalez/miTienditaSystem/repository/UserRepository.java
+++ src/main/java/com/alexandergonzalez/miTienditaSystem/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.alexandergonzalez.miTienditaSystem.repository;
 import com.alexandergonzalez.miTienditaSystem.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
