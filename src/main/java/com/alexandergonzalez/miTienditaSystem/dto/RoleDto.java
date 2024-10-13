d4e018e777e8bc1f725b557681ecec54bf357617
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-12 22:46:56 -0600

    Se agregó el constructor

--- src/main/java/com/alexandergonzalez/miTienditaSystem/dto/RoleDto.java
+++ src/main/java/com/alexandergonzalez/miTienditaSystem/dto/RoleDto.java
@@ -1,11 +1,13 @@
 package com.alexandergonzalez.miTienditaSystem.dto;
 
 import com.alexandergonzalez.miTienditaSystem.util.Role;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RoleDto {
     private Role role;
 }
