742cc27e1669531ed5b140d508fc9cd2803c3bc1
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-12 18:12:40 -0600

    Dto de role para poder actualizar el role

---
+++ src/main/java/com/alexandergonzalez/miTienditaSystem/dto/RoleDto.java
@@ -0,0 +1,11 @@
+package com.alexandergonzalez.miTienditaSystem.dto;
+
+import com.alexandergonzalez.miTienditaSystem.util.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RoleDto {
+    private Role role;
+}
